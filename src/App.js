62fd1c6d024369aa30b8bddff77caf7d97f02e53
Anastasiia99 <nastymaxy99@gmail.com>
2019-07-02 17:34:56 +0300

    Add eslintrc

--- src/App.js
+++ src/App.js
@@ -1,56 +1,47 @@
-import React from 'react';
-import {Button} from 'antd';
-import './App.css';
-import {getArticles} from './common/api.js';
+import React from "react";
+import { Button } from "antd";
+import "./App.css";
+import { getArticles } from "./common/api.js";
 
-
-class App extends React.Component{
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       error: null,
       isLoaded: false,
       items: []
-   };
+    };
   }
- 
+
   componentDidMount() {
-
-    getArticles()
-      
-      .then(
-        (result) => {
-          console.log(result);
-          this.setState({
-            isLoaded: true,
-            items: result.data
-          });
-        },
-        (error) => {
-          this.setState({
-             isLoaded: true,
-             error
-            });
-          }
-  )
+    getArticles().then(
+      result => {
+        console.log(result);
+        this.setState({
+          isLoaded: true,
+          items: result.data
+        });
+      },
+      error => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    );
   }
 
-  render(){ 
-    const {isLoaded, items } = this.state;
-    
-   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-        { isLoaded?items[0].title:<p>Loading...</p>
-            }
-        </p>
-        <Button type="primary">Button</Button>
-       
-      </header>
-    </div>
-  );
- }
+  render() {
+    const { isLoaded, items } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>{isLoaded ? items[0].title : <p>Loading...</p>}</p>
+          <Button type="primary">Button</Button>
+        </header>
+      </div>
+    );
+  }
 }
- export default App;
+export default App;
