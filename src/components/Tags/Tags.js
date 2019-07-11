95ed48231d44871aaa7f4288d727f5d706d13836
Anastasiia99 <nastymaxy99@gmail.com>
2019-07-11 09:20:03 +0300

    Add main icon

--- src/components/Tags/Tags.js
+++ src/components/Tags/Tags.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Layout, Row, Tag } from "antd";
-import { Link } from "react-router-dom";
+import { Layout, Select, Tag, Row, Col } from "antd";
+import { Link, withRouter } from "react-router-dom";
 import { getTags } from "../../common/api";
+import icon from "../../assets/icon.svg";
 
 const { Header } = Layout;
+const { Option } = Select;
 
 class Tags extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      width: 0,
+      height: 0,
       tags: []
     };
   }
@@ -19,7 +23,17 @@ class Tags extends React.Component {
         tags: result.data
       });
     });
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
   }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+  updateWindowDimensions = () => {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  };
+
   tagsList() {
     const { tags } = this.state;
     const listTags = tags.map(({ name }) => (
@@ -31,14 +45,63 @@ class Tags extends React.Component {
     ));
     return listTags;
   }
+  tagsSelect() {
+    const { tags } = this.state;
+    const listTags = tags.map(({ name }) => (
+      <Option value={name} key={name}>
+        <span>#{name}</span>
+      </Option>
+    ));
+    return listTags;
+  }
+  handleChange = value => {
+    console.log(`selected ${value}`);
+    this.props.history.push(`/${value}`);
+  };
 
   render() {
-    const tags = this.tagsList();
-    return (
-      <Header className="newsHeader">
-        <Row>{tags}</Row>
-      </Header>
-    );
+    const { width } = this.state;
+
+    if (width > 990) {
+      const tags = this.tagsList();
+      return (
+        <Header className="newsHeader">
+          <Row>
+            <Col span={2}>
+              <Link to="/">
+                <img className="mainIcon" alt="" src={icon} />
+              </Link>
+            </Col>
+            <Col span={22}>{tags}</Col>
+          </Row>
+        </Header>
+      );
+    } else {
+      const tags = this.tagsSelect();
+      return (
+        <Header className="newsHeader">
+          <Col span={2}>
+            <Link to="/">
+              <img className="mainIcon" alt="" src={icon} />
+            </Link>
+          </Col>
+          <Col
+            xs={{ span: 20, offset: 2 }}
+            md={{ span: 16, offset: 2 }}
+            lg={{ span: 14, offset: 3 }}
+          >
+            <Select
+              size="large"
+              placeholder="Select tag"
+              style={{ width: "100%" }}
+              onChange={this.handleChange}
+            >
+              {tags}
+            </Select>
+          </Col>
+        </Header>
+      );
+    }
   }
 }
-export default Tags;
+export default withRouter(Tags);
